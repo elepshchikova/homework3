b57214e9da6ae7c71ca0e8b43ba5105f72520d2c
elepshchikova <elepshchikova@mail.ru>
2022-09-16 00:06:52 +0300

    Решение задач 25,27,29

--- 123/Program.cs
+++ 123/Program.cs
@@ -1,1 +1,97 @@
-﻿
+﻿// Console.Write("Введите число a: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число b:  ");
+// int n = Convert.ToInt32(Console.ReadLine());
+ 
+// int num_n=1;
+// for(int i=0; i<n; i++)
+//  {
+//   num_n*=num;
+//  }
+//  Console.WriteLine("{0} ^ {1} = {2}", num, n, num_n);
+//  Console.ReadKey();
+// return 0;
+
+
+
+// Console.WriteLine("Введите число: ");
+// string num = Console.ReadLine();
+// int sum = 0;
+
+// for (int i = 0; i < num.Length; i++)
+// {
+//     int number = Convert.ToInt32(num[i].ToString());
+//     sum += number;
+// }
+// Console.WriteLine(sum);
+
+
+
+// int[] arr = new int[8];
+// for(int i = 0; i < 8; i++){
+//     arr[i] = new Random().Next(100);
+//     }
+// void PrintArray(int[] col){
+//     for(int count = 0; count < col.Length; count++){
+//         System.Console.WriteLine($"{col[count]},");
+//     }
+// }
+// PrintArray(arr);
+// System.Console.WriteLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
